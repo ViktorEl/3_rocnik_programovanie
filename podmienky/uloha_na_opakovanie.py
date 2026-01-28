cb561465f7def27fa6e6365c59301f9312ff4e3f
viktor <www.pista43@centrum.sk>
2026-01-28 14:19:44 +0100

    velke opakovanie funkcie

---
+++ podmienky/uloha_na_opakovanie.py
@@ -0,0 +1,5 @@
+# Definujte funkciu rozdiel(cislo1, cislo2), ktora na vstup 
+# zobrie 2 cisla a vypise vysledok
+# Po vytvoreni a otestovani funkcie uprajte kód tak, aby
+# si funkcia vypytala vstupy od pouzivatela
+# pomocky: print(), input(), cislo1 = float(cislo1)
--- podmienky/velke_opakovanie2.py
+++ podmienky/velke_opakovanie2.py
@@ -3,3 +3,19 @@
 # elif  ak hlavna podmienka nie je splnena kontroluje sa podmienka pod nou podmienkou elif
 # else  ak ani jedna z predchadzajucich podmienok nie je splnena vykona sa riadok else
 
+# Vytvorte/Definujte funkciu sucet(cislo1, cislo2), ktora ako
+# vstupne parametre zoberie dve cisla a vypise vysledok
+# vypytajte si vstupne parametre od pouzivatela
+
+def sucet(cislo1, cislo2):
+    vysledok = cislo1 + cislo2
+    print(vysledok)
+
+cislo1 = input('Zadajte prve cislo:') # toto je vstup od pouzivatela vypise text dole terminaly
+cislo1 = float(cislo1)
+cislo2 = input('zadajte druhe cislo:') # vstup od pouzivatela, vracia hodnotu v stringu (v retazci)
+cislo2 = float(cislo2) # pretoze sme pouzili prikaz input() tzn. vstup od pouzivatela
+# input nam stále ulozi do premennej string, preto ho musime pretypovat bud na float alebo int
+
+sucet(cislo1, cislo2)
+
